03f38e920854a2d35443e419a9491261998ea236
Jirawan <jirawan.333388@gmail.com>
2020-01-11 14:23:40 +0700

    add class java

--- src/profit/Salary.java
+++ src/profit/Salary.java
@@ -1,5 +1,37 @@
 package profit;
 
 public class Salary {
+	public double salary;
+	public double comisstion;
+	
+	public Salary(double salary, double comisstion) {
+		super();
+		this.salary = salary;
+		this.comisstion = comisstion;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public double getComisstion() {
+		return comisstion;
+	}
+
+	public void setComisstion(double comisstion) {
+		this.comisstion = comisstion;
+	}
+
+	@Override
+	public String toString() {
+		return "Salary [salary=" + salary + ", comisstion=" + comisstion + "]";
+	}
+	
+	
+	
 
 }
